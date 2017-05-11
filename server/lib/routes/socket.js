bfe49f57c538ddd24905bcd4e8324186dffab9a6
devpkteamf1 <devpkteamf1>
2017-05-11 21:39:15 +0000

    camera expt on raspberry pi

--- server/lib/routes/socket.js
+++ server/lib/routes/socket.js
@@ -3,7 +3,7 @@ var cv = require('opencv');
 // camera properties
 var camWidth = 320;
 var camHeight = 240;
-var camFps = 10;
+var camFps = 1;
 var camInterval = 1000 / camFps;
 
 // face detection properties
@@ -20,6 +20,8 @@ module.exports = function (socket) {
     camera.read(function(err, im) {
       if (err) throw err;
 
+      const ts = new Date().toLocaleTimeString();
+
       im.detectObject('./node_modules/opencv/data/haarcascade_frontalface_alt2.xml', {}, function(err, faces) {
         if (err) throw err;
 
@@ -27,9 +29,24 @@ module.exports = function (socket) {
           face = faces[i];
           im.rectangle([face.x, face.y], [face.width, face.height], rectColor, rectThickness);
         }
+	  
+	  if (faces.length > 0) {
+	      // im.save("img-"+ts+".jpg");
+	      console.log("captured");
+	  } else {
+	      console.log("NOT captured");
+	  }
 
-        socket.emit('frame', { buffer: im.toBuffer() });
-      });
+	  const font = "HERSEY_SIMPLEX";
+	  const color = [ 128, 127, 0];
+	  const scale = 1, thickness = 2;
+	  const msg = ts;
+	  const x = 10, y = 200;
+	  im.putText(msg, x, y, font, color, scale, thickness);
+
+          socket.emit('frame', { buffer: im.toBuffer() });
+	});
+	
     });
   }, camInterval);
 };
